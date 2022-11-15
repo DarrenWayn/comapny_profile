3a1122de23c08a2790ee744fca82fdab0af3cce7
Darren Wayn <dwayn.dev@gmail.com>
2022-11-15 14:15:58 +0700

    fixing logo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["storage.googleapis.com", "www.googleapis.com"],
+  },
 };
-
-module.exports = nextConfig;
--- pages/components/masthead.tsx
+++ pages/components/masthead.tsx
@@ -1,8 +1,12 @@
 import React, { useRef, useContext, useState, useCallback } from "react";
 import Image from "next/image";
 import { ScrollContext } from "@utils/scroll-observer";
+import Logo from "@assets/logo.png";
+import DownArrow from "@assets/down-arrow.png";
+import BgImage from "@assets/bg-img.jpg";
 
 const Masthead: React.FC = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
   const refContainer = useRef<HTMLDivElement>(null);
   const { scrollY } = useContext(ScrollContext);
 
@@ -12,16 +16,21 @@ const Masthead: React.FC = () => {
   if (elContainer) {
     progress = Math.min(1, scrollY / elContainer.clientHeight);
   }
+
+  const handleImageLoaded = useCallback(() => {
+    setImageLoaded(true);
+  }, []);
+
   return (
     <div
       ref={refContainer}
-      className="min-h-screen flex flex-col items-center justify-center"
+      className="min-h-screen flex flex-col items-center justify-center sticky top-0 z-[-1]"
       style={{
         transform: `translateY(-${progress * 20}vh)`,
       }}
     >
       <Image
-        src="/assets/bg-img.jpg"
+        src={BgImage}
         width={500}
         height={500}
         className="absolute w-full h-full object-cover"
@@ -29,24 +38,32 @@ const Masthead: React.FC = () => {
       />
       <div className={`flex-grow-0 pt-10 transition-opacity duration-1000 z-0`}>
         <Image
-          src="/assets/logo.png"
-          width={128 / 3}
+          src={Logo}
+          width={288 / 3}
           height={114 / 3}
           alt="logo"
+          priority
         />
       </div>
-      <div className="P-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,4)] text-center flex-1 flex items-center justify-center flex-col">
+      <div className="P-12 font-bold text-white drop-shadow-[0_5px_3px_rgba(0,0,0,4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl">Company Profile</h1>
         <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
           <span>This is a Test,</span> <span>done right.</span>
         </h2>
       </div>
-      <div className="flex-grow-0 pb-20 md:pb-10 transition-opacity duration-1000 z-0">
+      {/* <div className="flex-grow-0 pb-20 md:pb-10 transition-opacity duration-1000 z-1"> */}
+      <div
+        className={`flex-grow-0 pb-20 md:pb-10 transition-opacity duration-1000 z-0 ${
+          imageLoaded ? "opacity-100" : "opacity-0 -translate-y-10"
+        }`}
+      >
         <Image
-          src="/assets/logo.png"
+          src={DownArrow}
           width={188 / 3}
           height={105 / 3}
           alt="scroll down"
+          onLoad={handleImageLoaded}
+          priority
         />
       </div>
     </div>
