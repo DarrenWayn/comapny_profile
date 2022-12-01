f7659d3fdb06fa0c008505ff1f85b98df6251669
Darren Wayn <dwayn.dev@gmail.com>
2022-12-01 11:56:15 +0700

    exported nextConfig v5

--- next.config.js
+++ next.config.js
@@ -6,11 +6,4 @@ const nextConfig = {
     domains: ["robohash.org", "placeimg.com"],
   },
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
-
-module.exports = nextConfig;
