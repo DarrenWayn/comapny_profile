33a8645821368b9a58f781e3adc8f6f5a52c224f
Darren Wayn <dwayn.dev@gmail.com>
2022-12-01 11:33:36 +0700

    exported nextConfig v4

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["robohash.org", "placeimg.com"],
   },
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
